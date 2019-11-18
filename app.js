78e5cb8e32d04b3983e47d1e9168e0a8e89a2ec6
EC2 Default User <ec2-user@ip-172-31-28-159.us-east-2.compute.internal>
2019-11-18 23:36:45 +0000

    updating the users to user

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 
 
 var photonRouter = require('./routes/photon');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/user');
 var devicesRouter = require('./routes/devices');
 
 var app = express();
@@ -31,7 +31,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', usersRouter);
 app.use('/devices', devicesRouter);
 app.use('/photon', photonRouter);
 
