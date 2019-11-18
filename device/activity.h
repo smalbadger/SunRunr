c1da287a09b188710ea1ed77f8802ac6cd10a0f1
sawiltba <33639965+sawiltba@users.noreply.github.com>
2019-11-17 19:47:25 -0700

    create activity.h

---
+++ device/activity.h
@@ -0,0 +1,46 @@
+#ifndef ACTIVITY_H
+#define ACTIVITY_H
+
+#include <vector>
+
+#include "application.h"
+
+
+class GPS{
+ private:
+    float longitude;
+    float latitude;
+    float speed;
+    float uv;
+ 
+ public:
+    GPS();
+    GPS(float lon, float lat, float spe, float u);
+    float getLongitude();
+    float getLatitude();
+    float getSpeed();
+    float getUV();
+    
+};
+
+
+class Activity{
+    
+private:
+    std::vector<GPS> locations;
+    unsigned long duration;
+    String date;
+public:
+    Activity();
+    Activity(std::vector<GPS> loc, unsigned long dur);
+    std::vector<GPS> getGPS();
+    void add(GPS gps);
+    void setDuration(unsigned long dur);
+    String createJson();
+
+
+};
+
+
+
+#endif
