57db9554bb6629e335700c60f24cace508b87278
sawiltba <33639965+sawiltba@users.noreply.github.com>
2019-11-19 16:19:07 -0700

    duplicate devices req

--- routes/activity.js
+++ routes/activity.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 let User = require("../models/user");
 let Device = require("../models/device");
-let Device = require("../models/activity");
+let activity = require("../models/activity");
 
 let fs = require('fs');
 let bcrypt = require("bcryptjs");
