8d8c26936a48d45ef602dc057936a2b20d7e149c
smalbadger <smalbadger@email.arizona.edu>
2019-11-19 13:51:24 -0700

    quickFix

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ var db = require("../db");
 var userSchema = new db.Schema({
     email:        { type: String, required: true, unique: true },
     fullName:     { type: String, required: true },
-    passwordHash: { type: String, required: true ),
+    passwordHash: { type: String, required: true },
     lastAccess:   { type: Date, default: Date.now },
     userDevices:  { type:[String], default:[]},
     verified: { type: String, default: "disabled" }
