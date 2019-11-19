bb82a90ecc2ad4864ee33d24939e425456688617
zahrasadeq <33671943+zahrasadeq@users.noreply.github.com>
2019-11-18 21:13:26 -0700

    please work remove

--- routes/devices.js
+++ routes/devices.js
@@ -196,7 +196,7 @@ router.put('/replace', function(req, res, next) {
 //removing the device
 router.delete('/remove', function(req, res, next) {
     let responseJson = {
-        remove: false,
+        removed: false,
         message : "",
         apikey : "none",
         deviceId: "none"
@@ -235,26 +235,25 @@ router.delete('/remove', function(req, res, next) {
 
      // See if device is already registered
     Device.findOne({ deviceId: req.body.deviceId }, function(err, device) {
-        if (device == null) { //device not found
-            responseJson.message = "Device ID " + req.body.deviceId + "is not already registered.";
-            return res.status(400).json(responseJson);
-        }
-        else {
-
-            Device.remove({ deviceId: req.body.deviceId }, function(err, device) {
+        if (device !== null) { 
+                Device.remove({ deviceId: req.body.deviceId }, function(err, device) {
                 if (err) {
                     responseJson.message = err;
                     // This following is equivalent to: res.status(400).send(JSON.stringify(responseJson));
                     return res.status(400).json(responseJson);
                 }
                 else {
-                    responseJson.remove = true;
+                    responseJson.removed = true;
                     responseJson.apikey = deviceApikey;
                     responseJson.deviceId = req.body.deviceId;
                     responseJson.message = "Device ID " + req.body.deviceId + " was removed.";
                     return res.status(201).json(responseJson);
                 }
             });
+        }
+        else { //device not found
+            responseJson.message = "Device ID " + req.body.deviceId + "is not already registered.";
+            return res.status(400).json(responseJson);
         }
     });
 });
