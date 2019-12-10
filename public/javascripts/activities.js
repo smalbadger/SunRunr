ce6ddbbcf886cd4dcf11e75a144beec554c165ea
smalbadger <smalbadger@email.arizona.edu>
2019-12-10 02:01:34 -0700

    create activity map

--- public/javascripts/activities.js
+++ public/javascripts/activities.js
@@ -88,6 +88,7 @@ function showMap(){
   getByCurrId("uv-graph").slideUp()
 
   //TODO render map for current activity
+  map_out_path(getByCurrId("activity-map"), {});
 }
 
 function showSpeedGraph(){
@@ -140,43 +141,8 @@ window.onload = function () {
     getByCurrId("uvBtn").click(showUVGraph)
     showMap();
 
-    //All other activities are collapsed
+    //TODO: All other activities are collapsed
   })
-
-  // speedData = [
-  //   {y:0},
-  //   {y:11},
-  //   {y:12},
-  //   {y:10},
-  //   {y:12},
-  //   {y:10},
-  //   {y:9},
-  //   {y:7},
-  //   {y:12},
-  //   {y:6},
-  //   {y:7},
-  //   {y:8},
-  //   {y:7},
-  //   {y:7},
-  // ]
-  // uvData = [
-  //   {y:2.2},
-  //   {y:2},
-  //   {y:2.1},
-  //   {y:1.9},
-  //   {y:2.3},
-  //   {y:2.4},
-  //   {y:2},
-  //   {y:1.8},
-  //   {y:1},
-  //   {y:1.1},
-  //   {y:1.2},
-  //   {y:1.1},
-  //   {y:2.0},
-  //   {y:1.5},
-  // ]
-  // lineGraph("speed-graph", speedData, "Time", "Speed (MPH)", "Activity Speed");
-  // lineGraph("uv-graph", uvData, "Time", "UV Strength", "UV Exposure");
 }
 
 ////////////////////////////////////////////////////////////////////////////////
