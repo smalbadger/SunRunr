e37e537803070f7230fac51658a29a7d77262023
sawiltba <33639965+sawiltba@users.noreply.github.com>
2019-12-10 15:31:12 -0700

    moving UV to device

--- models/user.js
+++ models/user.js
@@ -6,8 +6,7 @@ var userSchema = new db.Schema({
     passwordHash: { type: String, required: true },
     lastAccess:   { type: Date, default: Date.now },
     userDevices:  { type:[String], default:[], sparse:true},
-    verified: { type: String, default: "disabled" },
-    uv: {type: Number, default: 10}
+    verified: { type: String, default: "disabled" }
 });
 
 var User = db.model("User", userSchema);
