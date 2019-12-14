ae935b82b901b785bab58d86fd98d021b9d5a785
smalbadger <smalbadger@email.arizona.edu>
2019-12-14 02:39:26 -0700

    adjusting for proper time zone on weather

--- public/javascripts/weather.js
+++ public/javascripts/weather.js
@@ -34,6 +34,7 @@ function prettyTime(uglyTime){
                       "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
   var d = new Date(uglyTime);
+  d.setHours(d.getHours-9)
   var amorpm = "pm";
   var hours = d.getHours();
   if (hours < 12) {amorpm = "am"}
