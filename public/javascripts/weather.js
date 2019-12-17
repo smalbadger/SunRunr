ecec1b1ca3bb63ab6c46a581b883b4ae53be3d2b
smalbadger <smalbadger@email.arizona.edu>
2019-12-17 14:54:08 -0700

    Bolded the location of the weather forecast

--- public/javascripts/weather.js
+++ public/javascripts/weather.js
@@ -67,7 +67,7 @@ function prettyDateTime(uglyTime, timezoneAdjustment){
 function createWeatherCard(val, timezone){
   $("#weather-forecast").append(
       "<div class='weather-card card teal lighten-1'>" +
-        "<div class='card-title weather-card-title'>" + prettyDateTime(val.dt_txt.replace(' ', 'T'), timezone) + "</div>" +
+        "<div class='card-title weather-card-title'><b>" + prettyDateTime(val.dt_txt.replace(' ', 'T'), timezone) + "</b></div>" +
         "<div class='row'>" +
           "<div class='col s6 card-image weather-icon-container'>" +
             "<img style='width:45px height:45px;' class='weather-icon' src='https://openweathermap.org/img/w/" + val.weather[0].icon + ".png'>" +
