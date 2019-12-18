c05fac1868938e8f4b07427c41bb0a0a392a9d77
zahrasadeq <33671943+zahrasadeq@users.noreply.github.com>
2019-12-17 18:36:21 -0700

    debugging

--- routes/activity.js
+++ routes/activity.js
@@ -74,7 +74,7 @@ router.get('/all', function(req, res, next) {
 });
 
 function parseISOString(s) {
-  var b = s.split(/\D+/);
+  var b = s.split('/\D+/');
   return new Date(Date.UTC(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]));
 }
 
