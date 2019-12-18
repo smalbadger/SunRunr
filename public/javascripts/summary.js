c6107aae143a46af5dd760bec3a801cfa4620d7d
smalbadger <smalbadger@email.arizona.edu>
2019-12-18 16:01:37 -0700

    summarize all user data

--- public/javascripts/summary.js
+++ public/javascripts/summary.js
@@ -31,6 +31,8 @@ function summarizeAllUsers(lat, lng, radius){
 
     $("#num-activities").text(data.activities.length.toString())
 
+    console.log(data.activities)
+
     // organize activities into users
     users = {}
     for(activity of data.activities){
@@ -41,14 +43,55 @@ function summarizeAllUsers(lat, lng, radius){
     }
 
     // calculate statistics
-    for (var key in users){
-      for (var activity of users[key]){
 
+    var avgTotDist = 0
+    var avgTotCals = 0
+    var avgUV = 0
+    for (var key in users){
+
+      var totalDist = 0
+      var totalCals = 0
+      var totalUV = 0
+      for (var activity of users[key]){
+        for (var i=0; i<activity.GPS.length-1; i++){
+          var p1 = activity.GPS[i]
+          var p2 = activity.GPS[i+1]
+          totalDist += milesBetween(p1.lat, p1.lon, p2.lat, p2.lon)
+          totalUV += activity.GPS[i].uv
+        }
+        totalUV += activity.GPS[activity.GPS.length-1].uv
+        totalCals += activity.calories
       }
+      avgTotDist += totalDist
+      avgTotCals += totalCals
+      avgUV += totalUV
     }
+    avgTotDist /= Object.keys(users).length
+    avgTotCals /= Object.keys(users).length
+    avgUV /= Object.keys(users).length
+
+    $("#average-total-distance").text(avgTotDist.toFixed(2) + " miles");
+    $("#average-total-calories").text(avgTotCals.toFixed(0) + " calories burned")
+    $("#average-uv").text(avgUV + " UV")
 
   })
   .fail(function (jqXHR, textStatus, errorThrown) {
     console.log("Error: "+testStatus.message)
   });
+}
+
+
+function milesBetween(lat1, lon1, lat2, lon2){
+  const metersPerMile = 1609.34;
+  const R = 6371e3; // metres
+  var phi1 = lat1 * Math.PI/180
+  var phi2 = lat2 * Math.PI/180
+  var deltaphi = (lat2-lat1) * Math.PI/180
+  var deltalambda = (lon2-lon1) * Math.PI/180
+  var a = Math.sin(deltaphi/2) * Math.sin(deltaphi/2) +
+          Math.cos(phi1) * Math.cos(phi2) *
+          Math.sin(deltalambda/2) * Math.sin(deltalambda/2);
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  var dist = R * c / metersPerMile;
+  return dist;
 }
