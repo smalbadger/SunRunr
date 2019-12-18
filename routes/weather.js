b489964f3dbb825186a32de3d4126cf8cedf65ce
smalbadger <smalbadger@email.arizona.edu>
2019-12-18 09:59:26 -0700

    removed debug statements from forecast endpoint

--- routes/weather.js
+++ routes/weather.js
@@ -3,15 +3,10 @@ let express = require('express');
 let router = express.Router();
 
 router.get("/forecast/:lat/:lon" , function(req, response) {
-
-  console.log(req.params)
-
   var key = "152d954ed997be2bb0784df77bdd7781";
   var lat = req.params.lat;
   var lon = req.params.lon;
   var url = `https://api.openweathermap.org/data/2.5/forecast?appid=${key}&lat=${lat}&lon=${lon}&units=Imperial`;
-
-  console.log(url);
   request(url, { json: true }, (err, res, body) => {
     if (err) {
       response.status(400).send(err);
