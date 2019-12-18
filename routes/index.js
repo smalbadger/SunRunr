82dc22a9d9f08514a6dd1fbc6eb0f4fee094675a
zahrasadeq <33671943+zahrasadeq@users.noreply.github.com>
2019-12-18 15:41:15 -0700

    o-auth

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,9 @@ var express = require('express');
 var router = express.Router();
 var path = require('path');
 
-/* GET home page. */
-//router.get('/', function(req, res, next) {
-//  res.render('userLogin', { title: 'SunRunr' });
-//});
+
 router.get('/',function(req,res){
- //res.sendFile(path.join('../public/userLogin.html'));
  res.sendFile('userLogin.html', { root: './public/'});
-// __dirname : It will resolve to your project folder.
 });
 
 module.exports = router;
