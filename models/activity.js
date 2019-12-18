db5d63a2b81c363ee9ee2a8fca4f82006d6cd5a1
sawiltba <33639965+sawiltba@users.noreply.github.com>
2019-12-17 18:03:14 -0700

    update of activity model to have aType

--- models/activity.js
+++ models/activity.js
@@ -9,8 +9,8 @@ var activitySchema = new db.Schema({
     calories: Number,
     temperature: Number,
     humidity: Number,
-    GPS: [{"lon":  Number, "lat":   Number, "speed": Number, "uv": Number}]
-
+    GPS: [{"lon":  Number, "lat":   Number, "speed": Number, "uv": Number}],
+    aType: {type: String, default: "walk"}
     //TODO: Add activity type field and set it to a default when the activity is
     //      posted. The default should be calculated via the activity speed.
 });
