460449a21986b97b4e81ab4b4e3ace3f3dd96df2
sawiltba <sawiltba@email.arizona.edu>
2019-12-18 10:05:39 -0700

    photon.js adding request

--- routes/photon.js
+++ routes/photon.js
@@ -129,7 +129,7 @@ router.post('/hit', function(req, res, next) {
         humidity: 0,
         };
 
-    let body = getCurrentWeather(lon, lat);
+    let body = await getCurrentWeather(lon, lat);
     console.log(body);
     
     if(body.hasOwnProperty(main)){
