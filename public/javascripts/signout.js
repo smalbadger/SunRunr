00cfb28be1db90b125d39a856681b6e51975cda6
smalbadger <smalbadger@email.arizona.edu>
2019-12-18 21:08:09 -0700

    put google meta tags everywhere

--- public/javascripts/signout.js
+++ public/javascripts/signout.js
@@ -4,9 +4,9 @@ $(function() {
 
       var auth2 = gapi.auth2.getAuthInstance();
       auth2.signOut().then(function () {
-      console.log('User signed out.');
+        console.log('User signed out.');
+      });
 
       window.location = "userLogin.html";
-    });
    });
 });
