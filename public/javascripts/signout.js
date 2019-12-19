abcf1973b7222fbd723b4feab5f03d16367adb6e
smalbadger <smalbadger@email.arizona.edu>
2019-12-18 21:40:11 -0700

    google shit

--- public/javascripts/signout.js
+++ public/javascripts/signout.js
@@ -1,12 +1,5 @@
 $(function() {
    $('.signout').click(function() {
       window.localStorage.removeItem('authToken');
-
-      var auth2 = gapi.auth2.getAuthInstance();
-      auth2.signOut().then(function () {
-        console.log('User signed out.');
-      });
-
-      window.location = "userLogin.html";
    });
 });
