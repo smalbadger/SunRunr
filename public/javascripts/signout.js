0b715211a20fe4babd03c172d999094e9475e93f
smalbadger <smalbadger@email.arizona.edu>
2019-12-18 21:16:48 -0700

    trying to log out still

--- public/javascripts/signout.js
+++ public/javascripts/signout.js
@@ -2,14 +2,10 @@ $(function() {
    $('.signout').click(function() {
       window.localStorage.removeItem('authToken');
 
-      try{
-        var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-          console.log('User signed out.');
-        });
-      } catch{
-        GoogleAuth.signOut();
-      }
+      var auth2 = gapi.auth2.getAuthInstance();
+      auth2.signOut().then(function () {
+        console.log('User signed out.');
+      });
 
       window.location = "userLogin.html";
    });
