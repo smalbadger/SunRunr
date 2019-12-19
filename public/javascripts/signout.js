64c30ad04013b3dd934de3172def7ac4c6f3203c
smalbadger <smalbadger@email.arizona.edu>
2019-12-18 20:55:14 -0700

    sign out of google auth

--- public/javascripts/signout.js
+++ public/javascripts/signout.js
@@ -2,5 +2,10 @@ $(function() {
    $('.signout').click(function() {
       window.localStorage.removeItem('authToken');
       window.location = "userLogin.html";
+
+      var auth2 = gapi.auth2.getAuthInstance();
+      auth2.signOut().then(function () {
+      console.log('User signed out.');
+    });
    });
 });
